5d77021ff44286d5a595b3af7f130c4effcc2369
golam-kibriya123 <golamkibriya622@gmail.com>
2023-03-15 10:24:14 -0500

    fixed submit button and display quize

--- index.js
+++ index.js
@@ -77,7 +77,10 @@ const displayQuiz = (data) => {
 };
 
 // EventListener for quiz submit button
-document.querySelector("#submit").addEventlistener("click", () => {
+document.querySelector("#submit").addEventListener("click", () => {
+  console.log('click');
+  console.log(answers)
+  console.log(answers.length)
   if (answers.length < 6) {
     return;
   }
